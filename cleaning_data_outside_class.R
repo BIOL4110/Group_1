997ed4626c0ada023c1730c82b02ba9ada64a63b
Emma Booth <ebooth03@uoguelph.ca>
2024-10-28 14:18:19 -0400

    histogram

--- cleaning_data_outside_class.R
+++ cleaning_data_outside_class.R
@@ -4,6 +4,7 @@ library(lubridate)
 library(readr)
 library(stringr)
 library(dplyr)
+library(ggplot2)
 
 # load csv file
 combined_data <- read_csv("combined_data.csv")
@@ -18,3 +19,26 @@ combined_clean_data <- combined_data %>%
 
 # View the cleaned dataset
 View(combined_clean_data)
+
+# remove all na. rows
+combined_clean_data <- na.omit(combined_clean_data)
+View(combined_clean_data)
+
+
+# Create the histogram for trophic position
+ggplot(combined_data, aes(x = trophic_position)) +
+  geom_histogram(binwidth = 0.5, fill = "blue", color = "black") +
+  labs(title = "Histogram of Trophic Position", x = "Trophic Position", y = "Frequency") +
+  theme_minimal()
+
+# Create the histogram for body mass
+ggplot(combined_data, aes(x = body_mass_in_grams)) +
+  geom_histogram(binwidth = 0.5, fill = "blue", color = "black") +
+  labs(title = "Histogram of Body Mass", x = "Body Mass", y = "Frequency") +
+  theme_minimal()
+
+# Create the histogram for Metabolic Rate
+ggplot(combined_data, aes(x = mr_raw_mg_per_kg_per_h)) +
+  geom_histogram(binwidth = 50, fill = "blue", color = "black") +
+  labs(title = "Histogram of Metabolic Rate", x = "Metabolic Rate", y = "Frequency") +
+  theme_minimal()
